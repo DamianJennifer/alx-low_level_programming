5d130c8d5d6937e6aa6527f0a3609d7d6abdf1fa
DamianJennifer <damianchinemerum@gmail.com>
2022-09-08 12:07:37 -0700

    jen c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main- Serves as the entry point for the program
- * Return: By default returns zero if no erros otherwise returns non-zero value.
+ * main - prints the phrase "Programming is like building a multilingual puzzle"
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
