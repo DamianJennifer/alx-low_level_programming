77cf1de0fbeab60d14a3d641cbaadd525d8b21d3
DamianJennifer <damianchinemerum@gmail.com>
2022-09-15 13:00:55 -0700

    bug

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,1 +1,12 @@
-
+#include "main.h"
+/**
+ * main - test function that print if integer isnegative or positive
+ * Return: 0 for success
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
